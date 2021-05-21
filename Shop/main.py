54a801258f6e703e9a01d7fdebe1b176cc010cc0
suchitpatil <suchitpatil29.5@gmail.com>
2021-05-21 19:19:57 +0530

    Added test cases for shop

--- Shop/main.py
+++ Shop/main.py
@@ -1,36 +1,61 @@
 from Shop.shop import Product
 from shop import *
 
+print("************ Welcome To Super Shop ************")
 # Testing Product Class
-sugar = Product(product_id=42, price=50)
-print(sugar.product_id, sugar.price, sugar.stock)
+sugar = Product(product_id=42, product_price=50)
+print(
+    "The Product id is {} , The Price of the product is {} And stock is {}".format(
+        sugar.product_id, sugar.product_price, sugar.stock
+    )
+)
 
-sugar.add_stock(quanitity=100)
-print(sugar.stock)
-sugar.decrease_stock(quanitity=50)
-print(sugar.stock)
+sugar.add_stock(quantity=100)
+print("The New added stock quantity is: {}".format(sugar.stock))
+sugar.decrease_stock(quantity=50)
+print("The decreased stock quantity is: {}".format(sugar.stock))
 
 sugar.set_price(75)
-print(sugar.price)
+print("The price of product is: {}".format(sugar.product_price))
 
 # Testing Customer Class
 customer = Customer(customer_id=123, balance=5000)
-print(customer.balance)
+print("The Current Customer Balance is: {}".format(customer.balance))
 
 customer.deposit_money(3500)
-print(customer.balance)
+print("The Deposited Money in Customer balance is: {}".format(customer.balance))
 
-customer.purchase(product_id=0, quantity=5)
-print(customer.balance)
+customer.purchase(product_price=10, quantity=5)
+print("After purchase of product the customer balance is: {}".format(customer.balance))
 
 # Testing Shop Class
-shop = Shop()
-print(shop.current_balance)
+shop = Shop(shop_balance=10000)
+print("The Shop Balance is: {}".format(shop.shop_balance))
 
 for i in range(5):
-    shop.add_product(product_id=i, price=(i+1)*20)
+    shop.add_product(product_id=i + 1, product_price=(i + 1) * 20)
 for i in range(5):
-    shop.add_customer(customer_id=i, balance=(i+1)*200)
+    shop.add_customer(customer_id=i + 1, product_id=i + 1, balance=(i + 1) * 200)
 
-shop.sell_product(product_id=0, customer_id=1, quantity=7)
-shop.purchase_stock(product_id=1, quanitity=15)
+# print items of product object of id 1 -initially
+print(shop.product_id_dict["1"].product_id)
+print(shop.product_id_dict["1"].product_price)
+print(shop.product_id_dict["1"].stock)
+print(shop.shop_balance)
+
+shop.purchase_stock(product_id=1, quantity=15)
+print(shop.product_id_dict["1"].product_id)
+print(shop.product_id_dict["1"].product_price)
+print(shop.product_id_dict["1"].stock)
+print(shop.shop_balance)
+print(shop.customer_id_dict["1"].balance)
+
+shop.sell_product(product_id=1, customer_id=1, quantity=10)
+
+print(shop.product_id_dict["1"].product_id)
+print(shop.product_id_dict["1"].product_price)
+print(shop.product_id_dict["1"].stock)
+print(shop.shop_balance)
+print(shop.customer_id_dict["1"].balance)
+
+print(shop.product_id_dict)
--- Shop/shop.py
+++ Shop/shop.py
@@ -1,11 +1,84 @@
-class Product():
-    def __init__(self):
-        pass
+class Product:
+    def __init__(self, product_id, product_price):
+        self.product_id = product_id  # which product
+        self.product_price = product_price  # product_price
+        self.stock = 0  # temporary stock will be zero
 
-class Customer():
-    def __init__(self):
-        pass
+    def add_stock(self, quantity):
+        self.stock = self.stock + quantity
 
-class Shop():
-    def __init__(self):
-        pass
+    def decrease_stock(self, quantity):
+        self.stock = self.stock - quantity
+
+    def set_price(self, price):
+        self.product_price = price
+
+
+class Customer:
+    def __init__(self, customer_id, balance):
+        self.customer_id = customer_id
+        self.balance = balance
+        self.customer_issued = -1
+
+    def deposit_money(self, money_deposit):
+        self.balance = self.balance + money_deposit
+
+    def purchase(self, product_price, quantity):
+        self.balance = self.balance - product_price * quantity
+
+
+class Shop:
+    def __init__(self, shop_balance):
+        self.shop_balance = shop_balance
+        self.product_id_dict = {}
+        self.customer_id_dict = {}
+
+    def add_product(self, product_id, product_price):
+        if str(product_id) in self.product_id_dict:
+            print("Product_Id  Already in use!")
+        else:
+            self.product_id_dict[str(product_id)] = Product(
+                product_id=product_id, product_price=product_price
+            )  # add new product.
+            print("Added Product id   ", product_id, product_price)
+
+    def add_customer(self, product_id, balance, customer_id):
+        if str(customer_id) in self.customer_id_dict:
+            print("Customer_Id  Already in use!")
+        else:
+            self.customer_id_dict[str(customer_id)] = Customer(
+                customer_id=customer_id, balance=balance
+            )  # add new product.
+            print("Added Customer id  ", customer_id, balance)
+
+    def purchase_stock(self, product_id, quantity):
+        # add quantity of product id
+        self.product_id_dict[str(product_id)].stock += quantity
+        # balance should get reduce from shop balance
+        self.shop_balance -= (
+            self.product_id_dict[str(product_id)].product_price * quantity
+        )
+
+    def sell_product(self, product_id, customer_id, quantity):
+        # check wether product is available or not
+        if str(product_id) in self.product_id_dict:
+            # check wether customer is added
+            if str(customer_id) in self.customer_id_dict:
+                # then sell product by multiplying product price into quantity
+                total_cost = (
+                    self.product_id_dict[str(product_id)].product_price * quantity
+                )
+                # reduce stock from shop
+                self.product_id_dict[str(product_id)].stock = (
+                    self.product_id_dict[str(product_id)].stock - quantity
+                )
+                # set customers product to customer id
+                self.customer_id_dict[str(customer_id)].customer_issued == product_id
+                # add balance in shop
+                self.shop_balance = self.shop_balance + total_cost
+                # total cost should be deducted from customer balance
+                self.customer_id_dict[str(customer_id)].balance -= total_cost
+            else:
+                print("Please Add Customer .")
+        else:
+            print("Product is not avilable , please add the product")
